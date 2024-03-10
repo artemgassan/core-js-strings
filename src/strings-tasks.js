f77b1edc457b19a661a8cdedc0b218ab61bdc8f0
artemgassan <artemgassan@ya.ru>
2024-03-10 14:58:04 +0300

    feat: finish invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -395,8 +395,16 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charAt(i) === str.charAt(i).toUpperCase()) {
+      result += str.charAt(i).toLowerCase();
+    } else {
+      result += str.charAt(i).toUpperCase();
+    }
+  }
+  return result;
 }
 
 /**
